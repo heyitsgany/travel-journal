de86e408493bd6c78f9fea4c9adfd152003048f1
heyitsgany <jasonheys@outlook.com.au>
2022-02-01 20:21:35 +1100

    added mobile responsive layout & changed data entries

--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 export default [
   {
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    title: "Tower Bridge",
+    location: "England",
+    googleMapsUrl: "https://goo.gl/maps/y3UjHYTwFcGSJ1gf8",
+    startDate: "10 Feb, 2021",
+    endDate: "14 Feb, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+      "Tower Bridge is one of London’s most famous landmarks. It is a Grade I listed combined bascule and suspension bridge, built between 1886 and 1894, designed by Horace Jones and engineered by John Wolfe Barry.",
+    imageUrl: "https://source.unsplash.com/UJiIuat1hZ0",
   },
 
   {
     title: "Sydney Opera House",
     location: "Australia",
     googleMapsUrl: "https://goo.gl/maps/dzLZ8KnhwSjzMGKw9",
-    startDate: "27 May, 2021",
+    startDate: "11 May, 2021",
     endDate: "9 Jun, 2021",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
@@ -22,13 +22,13 @@ export default [
   },
 
   {
-    title: "Geirangerfjord",
-    location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/tdFnnvKjxkJ8QLB56",
-    startDate: "01 Oct, 2021",
-    endDate: "18 Nov, 2021",
+    title: "Niagara Falls",
+    location: "Canada",
+    googleMapsUrl: "https://goo.gl/maps/sCeprc9XWukYoE7K8",
+    startDate: "05 Sep, 2021",
+    endDate: "18 Sep, 2021",
     description:
-      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+      "Niagara Falls is a group of three waterfalls at the southern end of Niagara Gorge, spanning the border between the province of Ontario in Canada, and the state of New York in the United States.",
+    imageUrl: "https://source.unsplash.com/NKqx9yvty1U",
   },
 ];
